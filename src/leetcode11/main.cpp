59ec5559a5f25e8dde996ca8ad57834c017254eb
yueyuanfeng <894599489@qq.com>
2020-09-21 02:56:03 -0700

    leetcode11

--- src/leetcode11/main.cpp
+++ src/leetcode11/main.cpp
@@ -1,63 +1,50 @@
 /*
-给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
 
-示例 1：
+说明：你不能倾斜容器，且 n 的值至少为 2。
 
-输入: "babad"
-输出: "bab"
-注意: "aba" 也是一个有效答案。
-示例 2：
+ 
 
-输入: "cbbd"
-输出: "bb"
+
+
+图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
 
 来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+链接：https://leetcode-cn.com/problems/container-with-most-water
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
 #include <iostream>
-#include <string>
-#include <string_view>
+#include <vector>
 
 using namespace std;
-class Solution {
-    bool isPalindrome(const string_view &s) {
-        if (s.empty()) {
-            return true;
-        }
-        size_t i = 0;
-        size_t j = s.size() - 1;
-        while (i < j) {
-            if (s[i] != s[j]) {
-                return false;
-            }
-            ++i;
-            --j;
-        }
-        return true;
-    }
+class Solution
+{
 public:
-    string longestPalindrome(string s) {
-        string_view sv(s);
-        int moveSize = sv.size();
-        while (moveSize >= 0) {
-            for (int i = 0, j = sv.size() - moveSize; i <= j; ++i) {
-                auto subSv = sv.substr(i, moveSize);
-                bool b = isPalindrome(subSv);
-                if (b) {
-                    return s.substr(i, moveSize);
-                }
+    int maxArea(vector<int> &height)
+    {
+        int i     = 0;
+        int j     = height.size() - 1;
+        int mArea = 0;
+        while (i < j) {
+            int area = (j - i) * min(height[i], height[j]);
+            mArea    = max(mArea, area);
+            if (height[i] > height[j]) {
+                --j;
             }
-            --moveSize;
+            else {
+                ++i;
+            }
         }
-        return string();
+
+        return mArea;
     }
 };
 
 int main()
 {
-    auto ans = Solution().longestPalindrome("");
+    vector<int> height{ 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+    auto ans = Solution().maxArea(height);
     cout << ans << endl;
     return 0;
 }
